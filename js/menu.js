9b6d3e17a629832a37b7777f3d1cf32e30c0eb28
gaetanodaidone <gaetanodaidone@gmail.com>
2019-05-01 02:12:58 -0400

    changed nav

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+// function myFunction(x) {
+//     x.classList.toggle("change");
     
-    document.getElementById('myDropdown').style.display = document.getElementById('myDropdown').style.display =='block' ?   'none' : 'block';
-}
+//     document.getElementById('myDropdown').style.display = document.getElementById('myDropdown').style.display =='block' ?   'none' : 'block';
+// }

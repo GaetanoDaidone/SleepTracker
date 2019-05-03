4861f493415ac17218f309d45f64e53936a5b872
gaetanodaidone <gaetanodaidone@gmail.com>
2019-05-02 22:13:46 -0400

    minor changes

---
+++ js/carousel2.js
@@ -0,0 +1,40 @@
+// var slides = [ "loveday.png","alfonso.png", "christopher.png", "gabe.png", "adrian.png", "christea.png", "oscar.png", "gaetano.png"];           
+                 
+//  var currentSlideIndex = 0;
+
+
+// function displaySlide() {
+//     var file = slides[ currentSlideIndex ];
+//     var url = "images/" + file;
+//     document.getElementById("slideshow").src = url;
+// }
+
+
+// // advance and display next slide
+// currentSlideIndex++;
+// if( currentSlideIndex >= slides.length ) {
+//     currentSlideIndex = 0;
+// }
+// displaySlide();
+
+// function nextSlide() {
+//         console.log(currentSlideIndex);
+//     currentSlideIndex ++;
+//     if( currentSlideIndex >= slides.length ) {
+//         currentSlideIndex = 0;
+ 
+//     }
+//     displaySlide();
+//     console.log(currentSlideIndex);
+// }
+
+// function prevSlide() {
+//     console.log("prev before clicked" + currentSlideIndex);
+//     currentSlideIndex --;
+//     if( currentSlideIndex < 0 ) {
+//         currentSlideIndex = 2;
+ 
+//     }
+//     displaySlide();
+//     console.log("prev after clicked" + currentSlideIndex);
+// }
